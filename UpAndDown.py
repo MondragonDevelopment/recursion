992e405ef727cf83a24e65b69b8544cd3d1a2b59
Mondragon <fimv705@gmail.com>
2023-07-22 13:18:30 -0600

    Count up to 30 and back down to the original number

---
+++ UpAndDown.py
@@ -0,0 +1,12 @@
+def countUpandDown(number):
+    print(number)
+    if number >= 30:
+        print('Reached the base case')
+        return
+    else:
+        countUpandDown(number + 1)
+        print(number, 'returning')
+        return
+
+
+countUpandDown(5)
